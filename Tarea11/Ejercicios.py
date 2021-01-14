eab1a66785ba2f80fd7b660e6e322aa6a134e62f
AndreaHGarrido <61398362+AndreaHGarrido@users.noreply.github.com>
2021-01-13 22:27:34 -0600

    Add files via upload

---
+++ Tarea11/Ejercicios.py
@@ -0,0 +1,53 @@
+def cuenta_regresiva(n):
+    print(n, end=" \n  ")
+    if n > 0:
+        return cuenta_regresiva(n-1)
+
+def main():
+    print("1er ejercicio. Cuenta regresiva: \n")
+    cuenta_regresiva(10)
+
+
+from pila_recursividad import Stack
+pila = Stack()
+pila_2 = Stack()
+
+def half_stack(mitad):
+    if pila.lenght()>0:
+        if pila.lenght()!= mitad:
+            pila_2.push(pila.pop())
+            half_stack(mitad)
+        elif pila.lenght()==mitad:
+            i = pila.pop()
+            print(f"\n {i} es el elemento en la posicion media de la pila.")
+            for elem in range(pila_2.lenght()):
+                pila.push(pila_2.pop())
+            print("\nResultado:")
+            return pila.to_string()
+    elif pila.lenght()==0:  #Si la longitud de la pila es igual a 0
+        print("La pila esta vacia, inserte elementos.")
+
+def half(size): #La función encuentra la mitad del tamaño de la pila
+    mitad = int(pila.lenght()/2)
+    residuo = pila.lenght()%2
+    if residuo == 0:
+        return int(mitad)
+    else:
+        mitad = mitad + residuo
+        return int(mitad)
+
+def main2():
+    print("\n2do Ejercicio. Seleccione los elementos medios de una pila: ")
+    pila.push(1)
+    pila.push(2)
+    pila.push(3)
+    pila.push(5)
+    pila.push(7)
+    pila.to_string()
+    print(f"Hay {pila.lenght()} elementos .  ")
+    size = pila.lenght() #Es el tamaño de la pila
+    mitad = half(size) #Regresa cuál es la posición media en la pila
+    half_stack(mitad)
+
+main()
+main2()
